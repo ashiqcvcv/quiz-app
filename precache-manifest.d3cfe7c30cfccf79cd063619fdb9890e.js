2c1a9d5035e9be00e0b75eba231ea905308c9a60
ashiqcvcv <ashiqc23@gmail.com>
2020-09-24 07:43:03 +0530

    Updates

---
+++ precache-manifest.d3cfe7c30cfccf79cd063619fdb9890e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ed20bbd36b400ac1a50d98937f4b7f9",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "d02d502655ba89e07c72",
+    "url": "/quiz-app/static/css/main.ac141c80.chunk.css"
+  },
+  {
+    "revision": "c3d79780ac53ccea4e00",
+    "url": "/quiz-app/static/js/2.7a0e306a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.7a0e306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d02d502655ba89e07c72",
+    "url": "/quiz-app/static/js/main.6c588d3a.chunk.js"
+  },
+  {
+    "revision": "b5a300e1fe90df755937",
+    "url": "/quiz-app/static/js/runtime-main.9459b062.js"
+  }
+]);
